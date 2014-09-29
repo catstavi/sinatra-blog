f03315543a2cc9d9e6d5d711ff9cfe60a3f0263e
Cate Uselton <cathode.use@gmail.com>
2014-09-28 20:27:09 -0700

    blog content displays on index automatically

--- lib/post.rb
+++ lib/post.rb
@@ -1,22 +1,29 @@
 class Post
-  attr_accessor :title, :url
+  attr_accessor :title, :url, :path
 
   def initialize(path)
-    @url = path
-    @title = format_title(path)
+    @path = path
+    set_url
+    @title = format_title(@url)
   end
 
   def self.all
     Dir["./views/posts/**.erb"].collect do |path|
-      path.slice!(0..6)
-      path.slice!(-4..-1)
       Post.new(path)
     end
+  end
+
+  def set_url
+    @url = @path[7..-5]
   end
 
   def format_title(path)
     title=path.slice(7..-1)
     title.capitalize!
     title.gsub("-"," ")
+  end
+
+  def read_file
+    File.read(@path)
   end
 end
