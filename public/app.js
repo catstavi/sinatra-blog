61992d8b77ff6f6d4d5e1057772c78e732685d11
Cate Uselton <cathode.use@gmail.com>
2014-10-01 14:15:00 -0700

    starting work on js navbar

---
+++ public/app.js
@@ -0,0 +1,12 @@
+$(window).scroll(function () {
+    if ($(window).scrollTop() > $(window).height()*.3) {
+        $('#scroller').css('top', $(window).scrollTop());
+        $('header').css('display', 'none');
+    }
+});
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() < $(window).height()*.3) {
+        $('header').css('display', 'block');
+    }
+});
