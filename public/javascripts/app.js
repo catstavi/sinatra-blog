e43b844332b1647054c1ad0a5c762e7e6dce7ce9
Cate Uselton <cathode.use@gmail.com>
2014-10-05 15:11:55 -0700

    show more and less works

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,3 +10,20 @@
 //         $('header').css('display', 'block');
 //     }
 // });
+
+if ( $('.show-options').siblings(".bpost").children(".after-intro").length > 0 ) {
+  console.log("moo");
+  $('.show-options').click(function () {
+    if ( $(this).siblings(".bpost").children(".after-intro").is(":visible") ) {
+      $(this).siblings(".bpost").children(".after-intro").hide();
+      $(this).html("&#x21a7;");
+    }
+    else {
+      $(this).siblings(".bpost").children(".after-intro").show();
+      $(this).html("&#x21bb;");
+    }
+  });
+}
+else {
+  $('.show-options').hide();
+}
